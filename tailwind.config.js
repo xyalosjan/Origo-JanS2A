6ca72a7af77a9c2b313a34a236c31aa5f34e851f
xyalosjan <56848341+xyalosjan@users.noreply.github.com>
2026-01-26 09:12:22 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./Areal-tabell.html",
+    "./Origo-Tabell.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
